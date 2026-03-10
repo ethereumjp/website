b5f512dd6eb0b89537b6a5c1928f7307b32d6f74
neila <40727091+neila@users.noreply.github.com>
2026-03-10 15:33:03 +0900

    settings

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,10 @@
 const path = require("path");
 
 const buildLintCommand = (filenames) =>
-  `npx yarn run biome lint ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" ")}`;
+  `npx yarn run biome lint ${filenames.map((f) => path.relative(process.cwd(), f)).join(" ")}`;
 
 const buildFormatCommand = (filenames) =>
-  `npx yarn run biome format ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" ")} `;
+  `npx yarn run biome format ${filenames.map((f) => path.relative(process.cwd(), f)).join(" ")}`;
 
 module.exports = {
   "**/*.{js,jsx,ts,tsx}": [buildLintCommand],
